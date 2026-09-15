ff25516415294bc9585e14a21d0c43114d9b8a48
DRAZEN I. NEPA <dnepa_250000000529@uic.edu.ph>
2026-09-15 14:47:53 +0800

    test code for cs beacuse save category not functioning

--- app.js
+++ app.js
@@ -74,3 +74,70 @@ function deleteCategory(button) {
     updateBadge();
 
 }
+DAVEY JAMES A. KHEE, Now
+const txtCatName = document.getElementById("txtCatName");
+const txtCatDesc = document.getElementById("txtCatDesc");
+const btnAdd = document.getElementById("btnAdd");
+
+const tableBody = document.getElementById("listIncomeCat");
+const categoryBadge = document.getElementById("categoryBadge");
+
+
+function updateBadge() {
+
+    const rowCount = tableBody.querySelectorAll("tr").length;
+
+    categoryBadge.textContent = Active Categories: ${rowCount};
+
+}
+
+
+btnAdd.addEventListener("click", function(){
+
+    const categoryName = txtCatName.value.trim();
+    const categoryDesc = txtCatDesc.value.trim();
+
+
+    if(categoryName === "" || categoryDesc === ""){
+        alert("Please fill in all fields.");
+        return;
+    }
+
+
+    const row = document.createElement("tr");
+
+
+    row.innerHTML = `
+        <td>${categoryName}</td>
+
+        <td>
+            ${categoryDesc}
+
+            <button class="btn btn-danger btn-sm float-end"
+            onclick="deleteCategory(this)">
+            Delete
+            </button>
+
+        </td>
+    `;
+
+
+    tableBody.appendChild(row);
+
+
+    updateBadge();
+
+
+    txtCatName.value = "";
+    txtCatDesc.value = "";
+
+});
+
+
+function deleteCategory(button){
+
+    button.closest("tr").remove();
+
+    updateBadge();
+
+}
